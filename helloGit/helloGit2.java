39daa789378b5791193cb518a524f4e76181271c
IgnacioSuarez <1995igna@gmail.com>
2024-08-19 08:49:59 +0200

    login

---
+++ helloGit/helloGit2.java
@@ -0,0 +1,8 @@
+public class helloGit2 {
+    public static void main(String[] args) {
+
+
+        System.out.println("¿Qué tal?");
+        System.out.println("New HelloGit");
+    }
+}
---
+++ helloGit/programa1.java
@@ -0,0 +1,12 @@
+public class programa1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola , esta es la primera línea");
+        System.out.println("g");
+        System.out.println("Otro commit más");;
+
+
+    }
+
+}
