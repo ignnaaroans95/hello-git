2df6514c6646849bdde7acad5931330a76d09aab
IgnacioSuarez <1995igna@gmail.com>
2024-08-20 09:54:19 +0200

    login v2

--- helloGit/hellogit3.java
+++ helloGit/hellogit3.java
@@ -1,7 +1,10 @@
-public class hellogit3 {
-
+public class helloGit3 {
     public static void main(String[] args) {
-        "hello git 3 v3"
-    }
 
+
+        System.out.println("Hola, este es un commit en el tercer ficherooooo");
+        System.out.println("hello git with changes");
+        System.out.println("Hello git 3vlogin");
+
+    }
 }
