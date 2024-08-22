72705a8f6c670c12ff003da1d4b3cfb61af74860
ignnaaroans95 <1995igna@gmail.com>
2024-08-22 10:41:08 +0200

    HelloGitHub

--- helloGit/helloGit2.java
+++ helloGit/helloGit2.java
@@ -2,7 +2,6 @@ public class helloGit2 {
     public static void main(String[] args) {
 
 
-        System.out.println("¿Qué tal?");
-        System.out.println("New HelloGit");
+        System.out.println("Esta es una linea para gitHub");
     }
 }
