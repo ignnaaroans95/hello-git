d7a6f6046b2a8763c1136bc2a47541ec0ccdff85
ignnaaroans95 <1995igna@gmail.com>
2024-08-28 09:32:31 +0200

    Actualizando hellogit2.java

--- helloGit2.java
+++ helloGit2.java
@@ -2,7 +2,6 @@ public class helloGit2 {
     public static void main(String[] args) {
 
 
-        System.out.println("¿Qué tal?");
-        System.out.println("New HelloGit");
+        System.out.println("Otro ejemplo para pull request");
     }
 }
